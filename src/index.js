fdf83418774df7b1fd96d2e5ed7cea4e836bb0cb
Lubna Naqvi <lubnanaqvi11@gmail.com>
2019-02-03 12:57:55 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import * as serviceWorker from './serviceWorker';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+        uid: '',
+        pwd: '',
+        loggedIn:false,
+        message:'Please enter a username and password'
+    };
+    this.clickHandler=this.clickHandler.bind(this);
+    this.changeHandler=this.changeHandler.bind(this);
+  }
+  changeHandler=(e)=>{this.setState({[e.target.id]:e.target.value})}
+  clickHandler=(e)=>{
+    if('Reactor'===this.state.uid&&'123'===this.state.pwd)
+      this.setState({message:'Welcome back Reactor',loggedIn:true});
+    else
+      this.setState({message:'Invalid username or password',loggedIn:false});
+  }
+  render() {
+    const style=this.state.loggedIn?{display:'none'}:{display:'block'}
+    return (
+      <div className="myApp">
+        <h4>{this.state.message}</h4>
+        <input id='uid' placeholder="Enter Username" onChange={this.changeHandler} style={style}/>
+        <br />
+        <input id='pwd' type="password" placeholder="Enter Password" onChange={this.changeHandler} style={style}/>
+        <br />
+        <button onClick={this.clickHandler}>Log in</button>
+      </div>
+    );
+  }
+}
+ReactDOM.render(<App />, document.getElementById('root'));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
